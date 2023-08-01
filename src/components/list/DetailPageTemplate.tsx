d31ddf069933c07879946b7a334773ff7d63f5d3
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-08-01 18:24:33 +0800

    add DetailPageTemplate component

---
+++ src/components/list/DetailPageTemplate.tsx
@@ -0,0 +1,44 @@
+import { useSearchParams } from 'expo-router';
+import { useState, useEffect } from 'react';
+
+import ListItemDetails from '~components/list/ListItemDetails';
+import Loading from '~components/Loading';
+
+const DetailPageTemplate = (getDetailsFunc, DetailsPropsType) => {
+  return () => {
+    const params = useSearchParams();
+
+    const title = params.title as string;
+
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [detailsData, setDetailsData] = useState<DetailsPropsType | null>(
+      null
+    );
+
+    const fetchData = async (title: string) => {
+      try {
+        setIsLoading(true);
+        const data = await getDetailsFunc(title);
+        if (data) {
+          setIsLoading(false);
+          setDetailsData(data);
+        }
+      } catch (e) {
+        console.log('error:', e);
+        setIsLoading(false);
+      }
+    };
+    useEffect(() => {
+      fetchData(title);
+    }, []);
+
+    return (
+      <>
+        {isLoading && <Loading />}
+        {detailsData && <ListItemDetails item={detailsData} />}
+      </>
+    );
+  };
+};
+
+export default DetailPageTemplate;
