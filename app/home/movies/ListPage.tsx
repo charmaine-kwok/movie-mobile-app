8f6f768d4f2ab935d40fcb04c91b1a53f51723f3
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-08-01 18:24:17 +0800

    import ListPageTemplate

--- app/home/movies/ListPage.tsx
+++ app/home/movies/ListPage.tsx
@@ -1,59 +1,9 @@
-import { useEffect, useState } from "react";
-import { useRouter } from "expo-router";
+import getMoviesList from '~functions/api/movie/getMoviesList';
+import { MovieProps } from '~functions/api/movie/getMoviesList';
+import ListPageTemplate from '~components/list/ListPageTemplate';
 
-import Loading from "~components/Loading";
-import getMoviesList from "~functions/api/movie/getMoviesList";
-import List from "~components/list/List";
-import { MovieProps } from "~functions/api/movie/getMoviesList";
-
-const ListPage: React.FC = () => {
-  const router = useRouter();
-  const [movieList, setMovieList] = useState<MovieProps[]>([]);
-  const [totalItem, setTotalItem] = useState<number | null>(null);
-  const [showLoadMoreButton, setShowLoadMoreButton] = useState<boolean>(false);
-
-  //pagination
-  const [currentPage, setCurrentPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const fetchData = async (currentPage: number) => {
-    try {
-      setIsLoading(true);
-      const data = await getMoviesList("movies", currentPage);
-      if (data) {
-        setIsLoading(false);
-
-        setMovieList((prev) => [...prev, ...data.items]);
-        // only need to do once
-        setTotalItem(data.totalItem);
-        setShowLoadMoreButton(data.totalPage > data.currentPage);
-      }
-    } catch (e) {
-      console.log("error:", e);
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchData(currentPage);
-  }, []);
-
-  const loadMoreData = () => {
-    setCurrentPage((prev) => prev + 1);
-    fetchData(currentPage + 1);
-  };
-
-  return (
-    <>
-      {isLoading && <Loading />}
-      <List
-        data={movieList}
-        totalItem={totalItem}
-        showLoadMoreButton={showLoadMoreButton}
-        loadMoreData={loadMoreData}
-      />
-    </>
-  );
-};
-
-export default ListPage;
+const MovieListPage = ListPageTemplate(
+  (page: number) => getMoviesList('movies', page),
+  [] as MovieProps[]
+);
+export default MovieListPage;
--- app/home/non-movies/ListPage.tsx
+++ app/home/non-movies/ListPage.tsx
@@ -1,59 +1,9 @@
-import { useEffect, useState } from "react";
-import { useRouter } from "expo-router";
+import getNonMoviesList from '~functions/api/non-movies/getNonMoviesList';
+import { NonMovieProps } from '~functions/api/non-movies/getNonMoviesList';
+import ListPageTemplate from '~components/list/ListPageTemplate';
 
-import Loading from "~components/Loading";
-import getNonMoviesList from "~functions/api/non-movies/getNonMoviesList";
-import List from "~components/list/List";
-import { NonMovieProps } from "~functions/api/non-movies/getNonMoviesList";
-
-const ListPage: React.FC = () => {
-  const router = useRouter();
-
-  const [movieList, setMovieList] = useState<NonMovieProps[]>([]);
-  const [totalItem, setTotalItem] = useState<number | null>(null);
-  const [showLoadMoreButton, setShowLoadMoreButton] = useState<boolean>(false);
-
-  //pagination
-  const [currentPage, setCurrentPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const fetchData = async (currentPage: number) => {
-    try {
-      setIsLoading(true);
-      const data = await getNonMoviesList(currentPage);
-      if (data) {
-        setMovieList((prev) => [...prev, ...data.items]);
-        // only need to do once
-        setTotalItem(data.totalItem);
-        setShowLoadMoreButton(data.totalPage > data.currentPage);
-      }
-      setIsLoading(false);
-    } catch (e) {
-      console.log("error:", e);
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchData(currentPage);
-  }, []);
-
-  const loadMoreData = () => {
-    setCurrentPage((prev) => prev + 1);
-    fetchData(currentPage + 1);
-  };
-
-  return (
-    <>
-      {isLoading && <Loading />}
-      <List
-        data={movieList}
-        totalItem={totalItem}
-        showLoadMoreButton={showLoadMoreButton}
-        loadMoreData={loadMoreData}
-      />
-    </>
-  );
-};
-
-export default ListPage;
+const NonMovieListPage = ListPageTemplate(
+  getNonMoviesList,
+  [] as NonMovieProps[]
+);
+export default NonMovieListPage;
