3671864e671fb88812aa42acb466f2c8b8f67271
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-08-01 18:25:22 +0800

    import DetailPageTemplate

--- app/home/non-movies/[title].tsx
+++ app/home/non-movies/[title].tsx
@@ -1,42 +1,10 @@
-import { useSearchParams } from "expo-router";
-import { useState, useEffect } from "react";
+import { NonMovieProps } from '~functions/api/non-movies/getNonMoviesList';
+import getNonMovieDetails from '~functions/api/non-movies/getNonMovieDetails';
+import DetailPageTemplate from '~components/list/DetailPageTemplate';
 
-import ListItemDetails from "~components/list/ListItemDetails";
-import { NonMovieProps } from "~functions/api/non-movies/getNonMoviesList";
-import Loading from "~components/Loading";
-import getNonMovieDetails from "~functions/api/non-movies/getNonMovieDetails";
+const NonMoviesDetailPage = DetailPageTemplate(
+  getNonMovieDetails,
+  {} as NonMovieProps
+);
 
-const DetailPage: React.FC = () => {
-  const params = useSearchParams();
-
-  const title = params.title as string;
-
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [movieData, setMovieData] = useState<NonMovieProps | null>(null);
-
-  const fetchData = async (title: string) => {
-    try {
-      setIsLoading(true);
-      const data = await getNonMovieDetails(title);
-      if (data) {
-        setIsLoading(false);
-        setMovieData(data);
-      }
-    } catch (e) {
-      console.log("error:", e);
-      setIsLoading(false);
-    }
-  };
-  useEffect(() => {
-    fetchData(title);
-  }, []);
-
-  return (
-    <>
-      {isLoading && <Loading />}
-      {movieData && <ListItemDetails item={movieData} />}
-    </>
-  );
-};
-
-export default DetailPage;
+export default NonMoviesDetailPage;
--- app/home/others/[title].tsx
+++ app/home/others/[title].tsx
@@ -1,42 +1,9 @@
-import { useSearchParams } from "expo-router";
-import { useState, useEffect } from "react";
+import { MovieProps } from '~functions/api/movie/getMoviesList';
+import getMovieDetail from '~functions/api/movie/getMovieDetails';
+import DetailPageTemplate from '~components/list/DetailPageTemplate';
 
-import ListItemDetails from "~components/list/ListItemDetails";
-import { MovieProps } from "~functions/api/movie/getMoviesList";
-import Loading from "~components/Loading";
-import getMovieDetail from "~functions/api/movie/getMovieDetails";
-
-const DetailPage: React.FC = () => {
-  const params = useSearchParams();
-
-  const title = params.title as string;
-
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [movieData, setMovieData] = useState<MovieProps | null>(null);
-
-  const fetchData = async (title: string) => {
-    try {
-      setIsLoading(true);
-      const data = await getMovieDetail("others", title);
-      if (data) {
-        setIsLoading(false);
-        setMovieData(data);
-      }
-    } catch (e) {
-      console.log("error:", e);
-      setIsLoading(false);
-    }
-  };
-  useEffect(() => {
-    fetchData(title);
-  }, []);
-
-  return (
-    <>
-      {isLoading && <Loading />}
-      {movieData && <ListItemDetails item={movieData} />}
-    </>
-  );
-};
-
-export default DetailPage;
+const OthersDetailPage = DetailPageTemplate(
+  (title: string) => getMovieDetail('others', title),
+  {} as MovieProps
+);
+export default OthersDetailPage;
