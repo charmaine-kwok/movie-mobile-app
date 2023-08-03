4e6c3af5539e2aca17dbe915b3a546a755bb4511
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-08-03 12:54:56 +0800

    update InputItem

--- src/components/input/InputItem.tsx
+++ src/components/input/InputItem.tsx
@@ -1,36 +1,46 @@
 import { Text, View, TextField } from "react-native-ui-lib";
 import { Controller } from "react-hook-form";
+import { useTranslation } from "react-i18next";
 
 const InputItem: React.FC<{
   name: string;
   label: string;
   control: any;
   errors: any;
-}> = ({ name, label, control, errors }) => {
+  numericKeyboard?: boolean;
+}> = ({ name, label, control, errors, numericKeyboard = false }) => {
+  const { t } = useTranslation();
+
   return (
     <View>
       <Controller
         control={control}
         rules={{ required: true }}
         render={({ field: { onChange, onBlur, value } }) => (
-          <View className="space-y-2">
-            <TextField
-              textColor
-              placeholder={label}
-              onBlur={onBlur}
-              onChangeText={onChange}
-              value={value}
-              label={label}
-              migrate
-            />
-          </View>
+          <TextField
+            textColor
+            placeholder={t(label)}
+            placeholderTextColor="rgba(172,169,169,0.7)"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            label={t(label)}
+            migrate
+            keyboardType={numericKeyboard ? "numeric" : "default"}
+            fieldStyle={{
+              borderBottomWidth: 1,
+              borderBottomColor: "gray",
+              marginTop: 5,
+              paddingBottom: 5,
+            }}
+          />
         )}
         name={name}
       />
       {errors[name] ? (
         <View className="h-[30px]">
           <Text red40 className="mt-1 font-semibold">
-            This is required.
+            {t("This field is required.")}
           </Text>
         </View>
       ) : (
