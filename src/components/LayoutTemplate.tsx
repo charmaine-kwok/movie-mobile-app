021fcb57e6b868714104cce919d1c5555bf65f03
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-08-03 12:48:17 +0800

    add AddItemPage screen

--- src/components/LayoutTemplate.tsx
+++ src/components/LayoutTemplate.tsx
@@ -1,10 +1,12 @@
-import { Stack } from 'expo-router';
-import { useTranslation } from 'react-i18next';
-import { useAtomValue } from 'jotai';
+import { Stack } from "expo-router";
+import { useTranslation } from "react-i18next";
+import { useAtomValue } from "jotai";
 
-import { DarkThemeAtom } from '~atoms/darkTheme';
+import { DarkThemeAtom } from "~atoms/darkTheme";
 
-export default function LayoutTemplate() {
+const LayoutTemplate: React.FC<{
+  type: "movies" | "others" | "non-movies";
+}> = ({ type }) => {
   const { t } = useTranslation();
 
   const isDarkTheme = useAtomValue(DarkThemeAtom);
@@ -13,18 +15,28 @@ export default function LayoutTemplate() {
     <Stack
       screenOptions={{
         headerStyle: {
-          backgroundColor: isDarkTheme ? '#000' : '#fff',
+          backgroundColor: isDarkTheme ? "#000" : "#fff",
         },
-        headerTintColor: isDarkTheme ? '#fff' : '#000',
-        contentStyle: { backgroundColor: isDarkTheme ? '#000' : '#fff' },
+        headerTintColor: isDarkTheme ? "#fff" : "#000",
+        contentStyle: { backgroundColor: isDarkTheme ? "#000" : "#fff" },
       }}
     >
       <Stack.Screen
-        name='index'
-        options={{ headerTitle: `${t('MovieList')}` }}
+        name="index"
+        options={{ headerTitle: `${t("MovieList")}` }}
       />
-      <Stack.Screen name='ListPage' />
-      <Stack.Screen name='[title]' options={{ headerTitle: `${t('Info')}` }} />
+      <Stack.Screen name="ListPage" />
+      <Stack.Screen name="[title]" options={{ headerTitle: `${t("Info")}` }} />
+      <Stack.Screen
+        name="AddItemPage"
+        options={{
+          headerTitle: `${t(
+            `Add ${type.charAt(0).toUpperCase() + type.slice(1, -1)} Item`,
+          )}`,
+        }}
+      />
     </Stack>
   );
-}
+};
+
+export default LayoutTemplate;
