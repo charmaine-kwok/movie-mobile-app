035b8aee2b9ea519d1e0853160bd5236df73753c
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-08-11 17:26:31 +0800

    update comment

--- src/functions/api/getItemDetails.tsx
+++ src/functions/api/getItemDetails.tsx
@@ -5,7 +5,7 @@ const getItemDetails = async <T extends TypeItem>(
   type: TypeCategory,
   item_id: string,
 ): Promise<T> => {
-  // Retrieving cert id list from API server
+  // Retrieving item details from API server
   const response = await fetch(
     `https://go-crud.fly.dev/api/${type}/${item_id}`, // production use
     // `http://localhost:8080/api/${type}/${item_id}`, // local use
