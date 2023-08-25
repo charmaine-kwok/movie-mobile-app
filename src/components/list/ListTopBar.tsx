cd61e8aba205380768d95b0f38c6b2de2b922068
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-08-25 11:00:31 +0800

    import fontSizeMap

--- src/components/list/ListTopBar.tsx
+++ src/components/list/ListTopBar.tsx
@@ -2,7 +2,13 @@ import { View, Text, TouchableOpacity } from "react-native-ui-lib";
 import { FontAwesome } from "@expo/vector-icons";
 import { useTranslation } from "react-i18next";
 import { Dispatch, SetStateAction } from "react";
+import { useAtomValue } from "jotai";
 
+import {
+  fontSizeMap,
+  smallerFontSizeMap,
+} from "~components/modal/FontSizeModal";
+import { fontSizeAtom } from "~atoms/fontSize";
 import NumberOfSelectedResults from "~components/numberOfResults/NumberOfSelectedResults";
 
 type ListProps = {
@@ -12,6 +18,8 @@ type ListProps = {
 
 const ListTopBar: React.FC<ListProps> = ({ totalItem, setIsVisible }) => {
   const { t } = useTranslation();
+
+  const fontSizeData = useAtomValue(fontSizeAtom);
 
   return (
     <View
@@ -24,10 +32,15 @@ const ListTopBar: React.FC<ListProps> = ({ totalItem, setIsVisible }) => {
           className="flex flex-row items-center"
           onPress={() => setIsVisible(true)}
         >
-          <Text textColor className="mr-1">
+          <Text textColor className="mr-2">
             <FontAwesome name="sort" size={24} />
           </Text>
-          <Text textColor>{t("Sort")}</Text>
+          <Text
+            textColor
+            className={`${fontSizeMap[fontSizeData]} sm : ${smallerFontSizeMap[fontSizeData]}`}
+          >
+            {t("Sort")}
+          </Text>
         </TouchableOpacity>
       </View>
     </View>
